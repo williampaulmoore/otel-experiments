3bece921dcc94b14f5070698b01bfa43eb384fc8
Paul Moore <williampaulmoore@gmail.com>
2023-06-03 14:32:29 +0100

    refactor: simplify environment teardown script

--- environment-setup/teardown-environment.sh
+++ environment-setup/teardown-environment.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
-# t(wpm). change this to used an array
+echo "Teardown environment"
 
-if docker ps --format ""{{.Names}} | grep -q "grafana"; then
-    docker stop grafana
-fi
+services=(
+    "grafana"
+    "prometheus"
+    "postgres"
+    "jaeger"
+    "otel-collector"
+    "fluentd"
+    "loki"
+)
 
-if docker ps --format "{{.Names}}" | grep -q "prometheus"; then
-    docker stop prometheus
-fi
+for s in ${services[@]}; do
 
-if docker ps --format "{{.Names}}" | grep -q "postgres"; then
-    docker stop postgres
-fi
+    if docker ps --format "{{.Names}}" | grep -q "$s"; then
+        docker stop "$s"
+    fi
 
-if docker ps --format "{{.Names}}" | grep -q "jaeger"; then
-    docker stop jaeger
-fi
+done
 
-if docker ps --format "{{.Names}}" | grep -q "otel-collector"; then
-    docker stop otel-collector
-fi
-
-if docker ps --format "{{.Names}}" | grep -q "fluentd"; then
-    docker stop fluentd
-fi
-
-if docker ps --format "{{.Names}}" | grep -q "loki"; then
-    docker stop loki
-fi
