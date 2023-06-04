cb7e255a5d8f8f83680391790219165b51051427
Paul Moore <williampaulmoore@gmail.com>
2023-06-04 21:24:55 +0100

    feat: move observability middleware to library

--- srv/ingest/api/src/server.ts
+++ srv/ingest/api/src/server.ts
@@ -1,10 +1,8 @@
 import { env } from '../env.mjs';
 import Koa from 'koa';
-import { OtelTracing, OtelMetrics } from './otel.js';
-import { FluentdLogging } from './fluentd';
 import router from './router';
 import { notfound } from './notfound';
-import {libName } from 'observability-otel-fluentd';
+import { OtelTracing, OtelMetrics, FluentdLogging } from 'observability-otel-fluentd';
 
 const app = new Koa();
 
@@ -16,6 +14,5 @@ app.use(router.allowedMethods());
 app.use(notfound);
 
 app.listen(env.LISTEN_ON_PORT, env.LISTEN_ON_ADDRESS,  () => {
-    console.log(libName());
     console.log(`Server running on http:://${env.LISTEN_ON_ADDRESS}:${env.LISTEN_ON_PORT}`);
 });
